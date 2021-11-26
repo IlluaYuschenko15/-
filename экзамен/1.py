559527be88d18e6337d9949397c3c5e18b34fbaa
IlluaYuschenko15 <74060295+IlluaYuschenko15@users.noreply.github.com>
2021-11-26 17:49:22 +0200

    Add files via upload

---
+++ экзамен/1.py
@@ -0,0 +1,57 @@
+#Задача 1
+#a = [1,1,2,3,5,8,13,21,34,55,89]
+#for i in a:
+    #if i<=5:
+        #print(i)
+    
+#Задача 2
+#Задача 3
+#numbers=[386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+    #399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217]
+#for i in numbers:
+   # if i%2==0:
+       # print(i)
+    #if i==237:
+        #break
+#Задача 4
+#age=int(input("Введите возвраст"))
+#if age<2:
+    #print("Младенец")
+#elif age>=2 and age<=4:
+   # print("МАлыш")
+#elif age>=4 and age<=13:
+    #print("Ребенок")
+#elif age>=13 and age<=20:
+   # print("Подросток")
+#elif age>=20 and age<=65:
+   # print("Взрослый")
+#else:
+    #print("Пожилой человек")
+#Задача 7
+#lst=[11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+#a=0
+#for i in lst:
+    #if i<30 and i%3==0:
+       #print(i)
+   # else:
+       # a+=1
+#print(a)
+#Задача6
+#a=['pizza', 'falafel', 'carrot cake']
+#b=['pizza', 'falafel', 'carrot cake']
+#c=input("Мороженое")
+#b.append(c)
+#for i in b:
+    #print(i)
+#Задача 8
+#list={'a': 2, 'b': 4, 'c': 6, 'd': 8}
+#for i in list.values():
+    #if i>2:
+       #print(i)
+#Задача 5
+#ne_stay_pop=[1,2,3,4,5,6,7,8,9]
+#stay_pop=["Andrey","Nasty","Kirill","Tima"]
+#for i in ne_stay_pop:
+    #stay_pop.append(i)
+#print(stay_pop)
+#Задача 9
